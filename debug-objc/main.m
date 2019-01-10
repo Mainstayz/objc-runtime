777e2fdbc05142dd0ad71a6c0525518bb1d2d115
hezongzhu <hezongzhu@gmail.com>
2019-01-11 00:58:55 +0800

    update

--- debug-objc/main.m
+++ debug-objc/main.m
@@ -7,10 +7,22 @@
 
 #import <Foundation/Foundation.h>
 
+@interface Foo : NSObject
+- (void)hello;
+@end
+@implementation Foo
+- (void)hello {
+    printf("你好\n");
+}
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        Foo *f = [Foo new];
+        [f hello];
+        [f hello];
+        
     }
     return 0;
 }
